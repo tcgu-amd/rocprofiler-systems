b6a7140bb423abc16a9f8561e74e5d4f87324eb5
Tim Gu <Tim.Gu@amd.com>
2025-03-21 17:47:07 -0400

    try 2

--- source/lib/rocprof-sys/library.cpp
+++ source/lib/rocprof-sys/library.cpp
@@ -444,7 +444,7 @@ rocprofsys_init_tooling_hidden()
     ROCPROFSYS_CONDITIONAL_BASIC_PRINT_F(_debug_init, "State is %s...\n",
                                          std::to_string(get_state()).c_str());
     
-    if(!_is_attach && (get_state() != State::PreInit || get_state() == State::Init || _once)) 
+    if(!_is_attach && (get_state() >= State::Active || _once)) 
         return false;
     _once = true;
 
