ccb572b3b305fc2538b5f4a1ba990bb74ecaf282
Tim Gu <Tim.Gu@amd.com>
2025-03-21 15:52:11 -0400

    more fix.

--- source/lib/rocprof-sys/library.cpp
+++ source/lib/rocprof-sys/library.cpp
@@ -422,7 +422,7 @@ rocprofsys_init_library_hidden()
     //Initialize attach
     if (tim::get_env("ROCPROFSYS_ATTACH", true))
     {
-        ROCPROFSYS_VERBOSE_F(1, "Initializing rocprof-sys in attach mode.\n")
+        ROCPROFSYS_VERBOSE_F(1, "Initializing rocprof-sys in attach mode.\n");
         config::set_signal_handler(&attach_detach_handler);
     }
 }
